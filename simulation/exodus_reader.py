c273632e88b3a5ceb53f4249e707d4aca81a5263
DomHazza <domfxh@gmail.com>
2023-07-07 16:42:38 +0100

    exodus_reader face only

--- simulation/exodus_reader.py
+++ simulation/exodus_reader.py
@@ -18,6 +18,24 @@ def scale(x, min_x, range_x):
     return (x-min_x)/range_x
 
 
+def plot_3D(x_values, y_values, z_values, colour_T):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(x_values, y_values, z_values, s=0.1, color=colour_T)
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    plt.show()
+    plt.close()
+    
+
+def T_to_colour(arr_T):
+    colour_T = []
+    for T in arr_T:
+        colour_T.append((0.0, 0.0, scale(T, min_T, range_T)))
+    return colour_T
+
+
 
 
 if __name__ == "__main__":
@@ -40,25 +58,20 @@ if __name__ == "__main__":
     min_T = np.min(temperatures)
     max_T = np.max(temperatures)
     range_T = max_T - min_T
+    
 
 
-    #Convert the temperatures to colours
-    colour_T = []
-    for T in temperatures:
-        colour_T.append((0.0, 0.0, scale(T, min_T, range_T)))
-
-
-    #Scatter the points
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter(x_values, y_values, z_values, s=0.1, color=colour_T)
-    ax.set_xlabel('X axis')
-    ax.set_ylabel('Y axis')
-    ax.set_zlabel('Z axis')
+    #COnsider the front face only
+    face_x = []
+    face_y = []
+    face_T = []
+    for i in range(len(x_values)):
+        if z_values[i] == 0.0:
+            face_x.append(x_values[i])
+            face_y.append(y_values[i])
+            face_T.append(temperatures[i])
+    
+    plt.scatter(face_x, face_y, color=T_to_colour(face_T), s=0.1)
     plt.show()
     plt.close()
-
-
-    #TODO The problem is now that we only need the front face
-
     
