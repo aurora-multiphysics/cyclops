c748a061f1c482cb3bcb966483ebd97b12981098
DomHazza <domfxh@gmail.com>
2023-07-14 18:22:29 +0100

    :hammer: face model GP 1st attempt

--- simulation/field_manager.py
+++ simulation/field_manager.py
@@ -1,54 +1,98 @@
-from gaussian_model import ExactGPModel
+from gaussian_model import NoiselessGPModel
 from exodus_reader import ExodusReader
 from matplotlib import pyplot as plt
-import torch
-
+from matplotlib import cm
+import numpy as np
 
 
 
 class FaceManager():
-    def __init__(self, face_x, face_y, face_T):
-        self.__face_T = torch.tensor(face_T)
-        self.__num_pos = self.__face_T.size()
+    def __init__(self, face_x_data, face_y_data, face_T_data, max_items):
+        if max_items < len(face_x_data):
+            face_x = np.zeros((max_items, 1))
+            face_y = np.zeros((max_items, 1))
+            face_T = np.zeros((max_items, 1))
+            for i in range(max_items):
+                rand_i = np.random.randint(0, len(face_x_data))
+                face_x[i] = face_x_data[rand_i]
+                face_y[i] = face_y_data[rand_i]
+                face_T[i] = face_T_data[rand_i]
+        else:
+            face_x = np.array(face_x_data).reshape(-1, 1)
+            face_y = np.array(face_y_data).reshape(-1, 1)
+            face_T = np.array(face_y_data).reshape(-1, 1)
 
-        self.__face_x = torch.tensor(face_x)
-        self.__face_y = torch.tensor(face_y)
-        
-    
+        face_pos = np.column_stack((face_x, face_y))
+
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        surf = ax.plot_trisurf(face_x_data, face_y_data, face_T_data, cmap=cm.jet, linewidth=0.1)
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+
+        self.__model = NoiselessGPModel(face_pos, face_T)
+
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        temp_data = self.__model.predict(np.column_stack((np.array(face_x_data).reshape(-1, 1), np.array(face_y_data).reshape(-1, 1))), std_out = False).reshape(-1)
+        print(temp_data)
+        surf = ax.plot_trisurf(face_x_data, face_y_data, temp_data, cmap=cm.jet, linewidth=0.1)
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+        plt.close()
+
+
+
+    def check_pos(self, pos):
+        x, y = pos[0], pos[1]
+        if x < -0.0112 or x > 0.0112 or y < -0.011 or y > 0.02 or (x**2 + y**2) < 0.06**2:
+            return False
+        return True
+
 
     def get_T(self, pos):
-        # Checks it is a valid position on the face
-        x_pos, y_pos = pos[0], pos[1]
-        if x_pos < -0.0115 or x_pos > 0.0115:
-            raise Exception("X pos out of range")
-        if y_pos < -0.0115 or y_pos > 0.0195:
-            raise Exception("Y pos out of range")
-        if x_pos**2 + y_pos**2 < 0.006**2:
-            raise Exception("In the circle")
-        
-        # Get closest coordinate
-        x_close = torch.square(self.__face_x - x_pos * torch.ones(self.__num_pos))
-        y_close = torch.square(self.__face_y - y_pos * torch.ones(self.__num_pos))
-        pos_index = torch.argmin(torch.add(x_close, y_close))
-        return face_T[pos_index]
+        if self.check_pos(pos) == False:
+            raise Exception("Invalid position")
+        else:
+            if len(pos) == 1:
+                return self.__model.predict(pos)[0], 0
+            else:
+                test_T, std_T = self.__model.predict(pos)
+                return test_T, std_T
 
-        
-    
-    def compare_T(self, arr_T):
-        return torch.sum(torch.abs(torch.sub(arr_T, self.__face_T)))
+
+
+
+
 
 
 
 class LineManager():
-    def __init__(self, line_y, line_T):
-        self.__model = ExactGPModel(torch.tensor(line_y), torch.tensor(line_T))
-        self.__model.learn(repetitions=100)
-        self.__model.plot(test_x=torch.linspace(-0.011, 0.019, 100))
+    def __init__(self, line_y_data, line_T_data, max_items):
+        if max_items < len(line_y_data):
+            line_y = np.zeros((max_items, 1))
+            line_T = np.zeros((max_items, 1))
+            for i in range(max_items):
+                rand_i = np.random.randint(0, len(line_y_data))
+                line_y[i] = line_y_data[rand_i]
+                line_T[i] = line_T_data[rand_i]
+        else:
+            line_y = np.array(line_y_data).reshape(-1, 1)
+            line_T = np.array(line_T_data).reshape(-1, 1)
+
+        self.__model = NoiselessGPModel(line_y, line_T)
+        self.__model.plot(x_test = np.linspace(-0.012, 0.02, 100).reshape(-1, 1))
 
 
     def get_T(self, pos):
-        return self.__model.predict(test_x=torch.tensor(pos)).mean.numpy()
+        if len(pos) == 1:
+            return self.__model.predict(pos)[0], 0
+        else:
+            test_T, std_T = self.__model.predict(pos)
+            return test_T, std_T
 
+
+    def check_pos(self, y):
+        if y < -0.11 or y > 0.02:
+            return False
+        return True
         
 
 
@@ -56,4 +100,11 @@ if __name__ == "__main__":
     reader = ExodusReader("monoblock_out.e")
     face_x, face_y, face_T = reader.front_only()
     line_y, line_T = reader.line_only(face_x, face_y, face_T)
-    manager = LineManager(line_y, line_T)
+    managerL = LineManager(line_y, line_T, 100)
+
+
+    print(managerL.get_T(np.array([[0.0]])))
+
+    print("\n")
+    managerF = FaceManager(face_x, face_y, face_T, 1000)
+
--- simulation/gaussian_model.py
+++ simulation/gaussian_model.py
@@ -1,76 +1,55 @@
-from matplotlib import pyplot as plt
-from tqdm import tqdm
-import gpytorch
-import torch
-
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RationalQuadratic
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 
 def f(x):
-    return torch.sin(x)
+    return np.exp(x)
 
 
 
-class ExactGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood=gpytorch.likelihoods.GaussianLikelihood()):
-        super().__init__(train_x, train_y, likelihood=likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-        self.optimiser = torch.optim.Adam(self.parameters(), lr=0.1)
-        self.likelihood = likelihood
-        self.__train_x = train_x
-        self.__train_y = train_y
+class NoiselessGPModel():
+    def __init__(self, x_train, y_train):
+        kernel = RationalQuadratic(length_scale=1)
+        self.__gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
+
+        self.__gp.fit(x_train, y_train)
+        self.__x_train = x_train
+        self.__y_train = y_train
+
     
+    def predict(self, x_test, std_out=True):
+        return self.__gp.predict(x_test, return_std=std_out)
 
-    def forward(self, x):
-        # Feed forward the input x
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+    
+    def plot(self, x_test = np.linspace(-5, 5, 100).reshape(100, 1)):
+        test_y, std_y = self.__gp.predict(x_test, return_std=True)
 
-
-    def learn(self, repetitions=500):
-        # Train the model
-        print("\nTraining...")
-        self.train()
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self)
-        for i in tqdm(range(repetitions)):
-            self.optimiser.zero_grad()
-            output = self(self.__train_x)
-            loss = -mll(output, self.__train_y)
-            loss.backward()
-            self.optimiser.step()
-
-
-    def predict(self, test_x = torch.linspace(-5, 5, 100)):
-        # Use the model to predict values
-        self.eval()
-        self.likelihood.eval()
-        with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            return self.likelihood(self(test_x))
-
-
-    def plot(self, test_x = torch.linspace(-5, 5, 100)):
-        # Plot the results of the model
-        with torch.no_grad():
-            test_y = self.predict(test_x)
-            lower, upper = test_y.confidence_region()
-
-            plt.plot(self.__train_x.numpy(), self.__train_y, 'k*')
-            plt.plot(test_x.numpy(), test_y.mean.numpy(), 'b')
-            plt.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
-            plt.legend(['Observed Data', 'Mean', 'Confidence'])
-            plt.show()
-            plt.close()
+        plt.plot(self.__x_train, self.__y_train, 'k*')
+        plt.plot(x_test, test_y, 'b')
+        plt.fill_between(
+            x_test.ravel(),
+            test_y - 1.96 * std_y,
+            test_y + 1.96 * std_y,
+            alpha=0.95,
+        )
+        plt.legend(['Observed Data', 'Mean', r'95% confidence interval'])
+        #plt.plot(x_test, f(x_test), 'k--')
+        plt.show()
+        plt.close()
 
 
 
+if __name__== "__main__":
+    #x_train = np.linspace(-5, 5).reshape(-1, 1)
+    #y_train = f(x_train)
+    x_train = np.array([0.011499999999999996, -0.011500000000000002, 0.009583333333333327, 0.010541666666666661, 0.007666666666666663, 0.008624999999999996, 0.0057499999999999956, 0.006708333333333329, 0.0038333333333333297, 0.004791666666666663, 0.001916666666666662, 0.0028749999999999956, -3.469446951953614e-18, 0.0009583333333333293, -0.0019166666666666707, -0.0009583333333333371, -0.0038333333333333353, -0.002875000000000003, -0.005750000000000002, -0.004791666666666668, -0.007666666666666669, -0.006708333333333335, -0.009583333333333333, -0.008625, -0.010541666666666668, 0.0125, 0.011999999999999999, 0.0135, 0.013000000000000001, 0.014499999999999999, 0.013999999999999999, 0.0155, 0.015, 0.0165, 0.016, 0.0175, 0.017, 0.0185, 0.018000000000000002, 0.0195, 0.019])
+    y_train = np.array([617.7810142261592, 170.6022114838236, 521.7502792402029, 568.8989262268101, 434.0389864587067, 476.6345507104152, 357.9193076223358, 394.309551443684, 296.316270176559, 325.16970344591056, 250.4265916926481, 271.4404941868866, 218.85814886792284, 233.03399846017842, 198.3958836666498, 207.4599397658493, 185.64295962080178, 191.234545552002, 177.9572861156409, 181.28869236112325, 173.5446931329063, 175.4183977941438, 171.2913781739981, 172.19144884991806, 170.76938841367416, 670.2457401119626, 643.8438526191087, 723.9169566473661, 696.9475635514357, 778.5447183940522, 751.1245377159483, 833.9347121516801, 806.1546527061431, 889.9389747620435, 861.8676598387348, 946.449324461157, 918.1362163868741, 1003.3922629105379, 974.8700538530743, 1060.7250688497088, 1032.0114913020122])
+    x_train = x_train.reshape(-1, 1)
+    y_train = y_train.reshape(-1, 1)
 
-
-if __name__ == "__main__":
-    train_x = torch.FloatTensor(10).uniform_(-5, 5)
-    train_y = f(train_x)
-
-    model = ExactGPModel(train_x, train_y)
-    model.learn()
-    model.plot()
+    gp = NoiselessGPModel(x_train, y_train)
+    gp.plot(x_test = np.linspace(-0.012, 0.02, 100).reshape(100, 1))
+    #gp.plot()
