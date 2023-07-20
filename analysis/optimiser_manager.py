f20f383c28b01ee8ac7be6c6ffaffb1ac835aeb9
DomHazza <domfxh@gmail.com>
2023-07-20 14:41:16 +0100

    struggling with a torch optimiser

--- analysis/optimiser_manager.py
+++ analysis/optimiser_manager.py
@@ -1,47 +1,60 @@
-from simulation.exodus_reader import ExodusReader
-from simulation.field_manager import FaceManager
-from analysis.loss_function import Loss
+from simulation.exodus_reader import ExodusManager
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 from torch import nn
+import numpy as np
 import torch
 
 
+
+OFFSET = [1, 2]
 
 def f(pos):
     return torch.sum(torch.sin(pos))
 
 
 
-BORDERS = torch.Tensor([[-0.012, 0.012], [-0.011, 0.02]])
-
-
+#Monoblock values
+BORDERS = torch.Tensor([[-0.0135, -0.0135], [0.0135, 0.0215]])
+RADIUS = 0.06
 
 
 class Explorer(nn.Module):
     def __init__(self, num_sensors, f):
         super().__init__()
-        pos = torch.distributions.Uniform(low = BORDERS[:0], high = BORDERS[:1]).sample((num_sensors,2))
-        self.__pos = nn.Parameter(pos)
+        positions = torch.distributions.Uniform(low = BORDERS[0], high = BORDERS[1]).sample((num_sensors,))
+        self.__positions = nn.Parameter(positions)
+        self.__zeros = torch.zeros(len(positions), 1)
         self.f = f       
 
 
     def forward(self):
-        return self.f(self.__pos)
+        return self.f(torch.cat((self.__positions, self.__zeros), -1))
+
+
+    def check_pos(self, pos):
+        if pos[0] < BORDERS[0, 0] or pos[0] > BORDERS[1, 0]:
+            return False
+        if pos[1] < BORDERS[0, 1] or pos[1] > BORDERS[1, 1]:
+            return False
+        if pos[0] **2 + pos[1] ** 2 < RADIUS ** 2:
+            return False
+        return True
 
 
     def __repr__(self):
-        return "f("+str(self.__pos)+") = "+str(self.f(self.__pos))
+        return "f("+str(self.__positions)+") = "+str(self.f(self.__positions))
 
 
 
 
 
-def training_loop(explorer, optimizer, n=10000):
+def training_loop(explorer, optimizer, n=100):
     losses = []
     for i in tqdm(range(n)):
         loss = explorer()
-        loss.backward()
+        print(loss)
+        #loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         losses.append(float(loss))  
@@ -51,13 +64,29 @@ def training_loop(explorer, optimizer, n=10000):
 
 
 
-if __name__ == "__main__":
-    reader = ExodusReader("monoblock_out.e")
-    face_x, face_y, face_T = reader.front_only()
-    managerF = FaceManager(face_x, face_y, face_T, 500)
-    loss = Loss(managerF)
 
-    explorer = Explorer(3, loss.loss_function)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    manager = ExodusManager('monoblock_out11.e')
+
+    explorer = Explorer(1, manager.get_point_temp)
     opt = torch.optim.Adam(explorer.parameters(), lr=0.001)
 
     print("\n\nOptimising...")
--- simulation/exodus_reader.py
+++ simulation/exodus_reader.py
@@ -1,35 +1,96 @@
 from matplotlib import pyplot as plt
+from matplotlib import cm
 import chigger
 import torch
 import os
 
 
 
+#Monoblock values
+X_BOUNDS = (-0.0135, 0.0135)
+Y_BOUNDS = (-0.0135, 0.0215)
+Z_BOUNDS = (0, 0.012)
 
 
+
+class ExodusManager():
+    def __init__(self, relative_path):
+        # Load the file
+        absolute_path = os.path.dirname(__file__)
+        full_path = os.path.join(absolute_path, relative_path)
+        reader = chigger.exodus.ExodusReader(full_path)
+        reader.update()
+
+        self.__result = chigger.exodus.ExodusResult(reader, variable='temperature', viewport=[0,0,0.5,1])
+        self.__result.update()
+
+        self.__compare_x, self.__compare_y, self.__compare_T = self.create_comparison()
+
+
+    def get_line(self, point_1, point_2, num_points = 200):
+        # Find the temperature of a line of points
+        sample = chigger.exodus.ExodusResultLineSampler(self.__result, resolution = num_points, point1 = point_1, point2 = point_2)
+        sample.update()
+        x = sample[0].getDistance()
+        y = sample[0].getSample('temperature')
+        return x, y
+    
+
+    def plot_1D(self):
+        # Plot a graph of a 1D line
+        x_values, y_values = self.get_line((0.013, -0.0135, 0), (0.013, 0.0215, 0))
+        plt.plot(x_values, y_values)
+        plt.show()
+        plt.close()
+
+    
+    def get_point_temp(self, pos):
+        # Get the temperature at a point
+        sample = chigger.exodus.ExodusResultLineSampler(self.__result, resolution=1, point1 = pos, point2 = pos)
+        sample.update()
+        temp_value = sample[0].getSample('temperature')
+        return torch.Tensor([temp_value[0]])
+
+
+    def create_comparison(self, num_x = 40, num_y = 40):
+        # Find the points on the surface of the monoblock to compare the predicted temperature & actual temperature at
+        x_values = []
+        y_values = []
+        T_values = []
+        for x in torch.linspace(X_BOUNDS[0], X_BOUNDS[1], num_x):
+            for y in torch.linspace(Y_BOUNDS[0], Y_BOUNDS[1], num_y):
+                if self.check_face((x, y, 0)):
+                    x_values.append(x)
+                    y_values.append(y)
+                    T_values.append(self.get_point_temp([x, y, 0]))
+        return torch.Tensor(x_values), torch.Tensor(y_values), torch.Tensor(T_values)
+
+
+    def plot_3D(self):
+        # Plot a smart 3D graph of the temperature at various points of the monoblock
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        surf = ax.plot_trisurf(self.__compare_x, self.__compare_y, self.__compare_T, cmap=cm.jet, linewidth=0.1)
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+        plt.close()
+        
+
+    def check_face(self, pos):
+        # Check if a point is not in the monoblock's hole
+        if pos[0]**2 + pos[1] ** 2 < 0.006**2:
+            return False
+        return True
+
+    
+    def get_comparison_positions(self):
+        # Return the comparison positions
+        return self.__compare_x, self.__compare_y
+
+
+    
 
 
 
 if __name__ == "__main__":
-    relative_path = 'monoblock_out11.e'
-    absolute_path = os.path.dirname(__file__)
-    full_path = os.path.join(absolute_path, relative_path)
-    reader = chigger.exodus.ExodusReader(full_path)
-    reader.update()
-
-    result = chigger.exodus.ExodusResult(reader, variable='temperature', viewport=[0,0,0.5,1])
-    cbar = chigger.exodus.ExodusColorBar(result)
-    result.update()
-
-    sample = chigger.exodus.ExodusResultLineSampler(result, resolution=200, point1 = (0, -0.015, 0), point2 = (0.0, 0.02, 0.0))
-    sample.update()
-
-    x = sample[0].getDistance()
-    y = sample[0].getSample('temperature')
-
-    line = chigger.graphs.Line(x, y, width=4, label='probe')
-    graph = chigger.graphs.Graph(line, yaxis={'lim':[0,1600]}, xaxis={'lim':[0,0.04]}, viewport=[0.5,0,1,1])
-
-    plt.plot(x, y)
-    plt.show()
-    plt.close()
+    manager = ExodusManager('monoblock_out11.e')
+    manager.plot_3D()
