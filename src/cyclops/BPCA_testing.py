69f2cb065492c54d2783f2be31438323adffae7c
CBqk3762 <ciara.byers@ukaea.co.uk>
2023-12-15 16:51:40 +0000

    Problems with the transformed bounary conditions, valid before transform but not afterwards. Must be an error in algorithm implementation

--- src/cyclops/BPCA_testing.py
+++ src/cyclops/BPCA_testing.py
@@ -91,12 +91,15 @@ def pad_to_subtract(matrix1, matrix2):
         zeros_to_add = rows2 - rows1
         padded_matrix1 = np.pad(matrix1, pad_width=((0, zeros_to_add), (0, 0)))
         padded_matrix2 = matrix2
-        print(padded_matrix1)
+        #print(padded_matrix1)
     # Pad matrix2 with rows to match matrix1 if it has more
     elif rows1 > rows2:
         zeros_to_add = rows1 - rows2
         padded_matrix1 = matrix1
         padded_matrix2 = np.pad(matrix2, pad_width=((0, zeros_to_add), (0, 0)))
+    else:
+        padded_matrix1 = matrix1
+        padded_matrix2 = matrix2
 
     # Pad matrix1 with columns if matrix2 has more
     if cols1 < cols2:
@@ -113,57 +116,64 @@ def pad_to_subtract(matrix1, matrix2):
 def update_a(A: np.array, B: np.array, X: np.array, I: float):
     """Function updates row I of matrix A according to BPCA algorithm"""
 
-    #Calculating the constant for row being updated, I
-    #const is given by mod(X_noi - A_noi*B.T)^2
-    X_noi = np.delete(X, I, 0)
-    A_noi = np.delete(A, I, 0)
-    print(A_noi.shape, "A_noi")    
-    print(X_noi.shape, "X_noi")
+    print("Run", I, " X shape ", X.shape)
+    #Start from LSI problem
+    def calc_top_const(A: np.array, B: np.array, X: np.array, I: float):
+        """Function calculates the value of the constant in the initial
+        minimisation problem for a row I"""
+        #Calculating the constant for row being updated, row I
+        #const is given by mod(X_noi - A_noi*B.T)^2
+        X_noi = np.delete(X, I, 0)
+        A_noi = np.delete(A, I, 0)
+        #Pad A_noi and/or B to allow matrix multiplication
+        A_noi, B_T = pad_matrices(A_noi, B.T)
+        AB_T = np.matmul(A_noi,B_T)
+        #Pad X_noi/AB to allow matrix subtraction
+        X_noi, AB_T = pad_to_subtract(X_noi, AB_T)
+        inner = np.subtract(X_noi, AB_T)
+        In0 = inner.shape[0]
+        In1 = inner.shape[1]
+        if In0>In1:
+            inner = np.pad(inner, pad_width=((0,0), (0, In0 - In1))) 
+        elif In1>In0:
+            inner = np.pad(inner, pad_width=((0, In1 - In0), (0,0)))
 
-    bshape = B.shape[0]
-    print(B.shape)
-    #Pad A_noi/B to allow matrix multiplication
-    A_noi, B = pad_matrices(A_noi, B)
-    AB = np.matmul(A_noi,B)
+        inner = drop_null_cols(inner)
+        row_const = np.linalg.norm(inner)
+        return row_const
 
-    #Pad X_noi/AB to allow matrix subtraction
-    X_noi, AB = pad_to_subtract(X_noi, AB)
-    inner = np.subtract(X_noi, AB)
-    In0 = inner.shape[0]
-    In1 = inner.shape[1]
-    if In0>In1:
-        inner = np.pad(inner, pad_width=((0,0), (0, In0 - In1))) 
-    elif In1>In0:
-        inner = np.pad(inner, pad_width=((0, In1 - In0), (0,0)))
-
-    null_cols = []
-    for col in range(inner.shape[1]):
-        if sum(inner[:,col]) == 0:
-            null_cols.append(col)
-    
-    inner = np.delete(inner, null_cols, axis=1)
-    print(inner.shape)
-
-    row_const = np.linalg.norm(inner) # Constant to be added 
-    print(row_const)
     #Remeber to come back and change U = B to avoid accidental linking
+    #seeking optimal row A[I] in boundaries now
     U = B
     W = A[I]
     V = check_shape(X[I])
     G = np.c_[B, -B]
     h = np.c_[X[I], -X[I]]
 
-    #S = Qy-P_i.T*v
-    #now have LDP problem minimise s**2 + p_2.T*v s.t. GRQ^1s > h -GRQ^-1P_1'*v
-    #Convert the LDP problem to an NNLS one 
-    E, n, min_S_sq, LDP_const, f = LSI_to_LDP(G, h, U, W, V)
+    #As in paper, let S = Qy-P_i.T*v to get to a LDP problem as follows
+    #min||s||**2 + ||p_2.T*v||**2 s.t. GRQ^1s > h -GRQ^-1P_1'*v
+    E, n, min_S_sq, LDP_const, f, new_bounds = LSI_to_LDP(G, h, U, W, V)
     #E, f = pad_to_subtract(E, f)
     f_len = f.shape[0]
     E_len = E.shape[0]
     E_pad = np.pad(E, pad_width=((0, f_len - E_len), (0, 0)))
-    r, rnorm = nnls(E_pad, f)
-    print("r", r)
-    print("rnorm", rnorm)
+    solution_vec, resid = nnls(E_pad, f)
+    #E_pad, solu_vec_pad = pad_matrices(E_pad, solution_vec)
+    E_sol = np.matmul(E_pad, solution_vec)
+
+    #E_sol = drop_null_rows(E_sol)
+    #print("E_sol", E_sol.shape)
+    #solution_vec = drop_null_cols(solution_vec)
+    #print("solution_vec", solution_vec.shape)
+    #print("f", f.shape)
+    if E_sol.shape[0] >= f_len:
+        zeros = np.zeros(E_sol.shape[0] - f_len)
+        f_pad = np.hstack((f, zeros))
+    #    print(f_pad.shape)
+    r = np.subtract(E_sol, f_pad)
+    #print("r", r)
+    
+    return r, resid
 
 
 def LSI_to_LDP(G: np.array, h: np.array, U: np.array, W: np.array, V: np.array):
@@ -177,110 +187,149 @@ def LSI_to_LDP(G: np.array, h: np.array, U: np.array, W: np.array, V: np.array):
     # min w ||Uw -v||**2 s.t. Gw>=h
 
     #Decompose matrix U
-    P, Q, R_t = np.linalg.svd(U, full_matrices=False) 
+    P, Q, R_t = np.linalg.svd(U, full_matrices=True) 
+    Q = diagsvd(Q, P.shape[0], R_t.shape[-1])
+    print(P.shape, Q.shape, R_t.shape)
     P_shape = P.shape[1]
     R_t_shape = R_t.shape[0]  
     Q_shape = Q.shape
     Q0 = Q_shape[0]
-
-    if len(Q_shape) == 1:
-        #Need to check if Q can be of the form (,n) not (n,)
-        Q = check_shape(Q)
-        Q1 = Q.shape[1]
-        Q_padded1 = np.pad(Q, pad_width=((0,0), (0, R_t_shape-Q1) )) 
-        print("Q", Q.shape) 
-    else:
-        Q_padded1 = np.pad(Q, pad_width=((0,P_shape - Q_shape[0]), (0, R_t_shape - Q_shape[1]) )) 
-        print("Q", Q.shape) 
-
-    hshape = h.shape[1]
-    gshape = G.shape[1]
+ 
+    W = check_shape(W)
+    R_t, W = pad_matrices(R_t, W)
     y = np.matmul(R_t, W)
-    y = check_shape(y)
-    yshape = y.shape
-    h = np.pad(h, pad_width=((0,0), (0, gshape - hshape) ))
+    hshape = h.shape[0]
+    gshape = G.shape[0]
+    print("h.T,G.T", h.T.shape, G.T.shape)
+    h_padded = np.pad(h, pad_width=((0, gshape - hshape), (0,0) ))
+    print("h_padded.T", h_padded.T.shape)
+
     #Let s = Qy-P_1.T v  s.t. our problem is now min s ||s||**2 ||P_2.T v||**2
-    # s.t. GRQ^-1 s >=h - GRQ^-1 P_1.T v
-    E = np.hstack((G.T, h.T))
-    E = E.T
-    P_1 = P[:R_t_shape+1,:] 
-    P_2 = P[R_t_shape+1:,:]
-    P1_shape0 = P_1.shape[0]
-    P2_shape0 = P_2.shape[0]    
-
-    V0 = V.shape[0]
-    V1 = V.shape[1]
-    if P1_shape0 > V0:
-       V_padded1 = np.pad(V, pad_width=((0,P1_shape0 - V0), (0, 0) ))
-       P_1_padded = P_1
-    elif P1_shape0 < V0:
-       V_padded1 = V
-       P_1_padded = np.pad(P_1, pad_width=((0, V0 - P1_shape0), (0, 0)))
-
-    Qy = np.matmul(Q_padded1, y)
-    P1TV = np.matmul(P_1.T, V_padded1)
+    # s.t. GR(Q^-1)*s + GR(Q^-1)*(P_1.T)*v>=h
+    E = np.vstack((G.T, h_padded.T))
+    print("E", E.shape)
+    #Note the shapes of P_1 and P_2 are (nxm) and (nx(n-m)) where P from 
+    # earlier is (nxn) and R_t is (mxm)
+    P_1 = P[:, :R_t_shape] 
+    P_2 = P[:, :(P.shape[0] - R_t_shape)] 
+#CURRENT EDITING POINT
+    P_1_padded_T, V_padded1 = pad_matrices(P_1.T, V)
+    Qy = np.matmul(Q, y)
+    P1TV = np.matmul(P_1_padded_T, V_padded1)
     S_LDP = np.subtract(Qy, P1TV)
     min_S_sq = np.square(np.linalg.norm(S_LDP))
+    print(min_S_sq)
 
-    if P2_shape0 > V0:
-       V_padded2 = np.pad(V, pad_width=((0,P2_shape0 - V0), (0, 0) ))
-       P_2_padded = P_2
-    elif P2_shape0 < V0:
-       V_padded2 = V
-       P_2_padded = np.pad(P_2, pad_width=( (0, V0 - P2_shape0) , (0, 0)))
-  
-    P2TV = np.matmul(P_2_padded.T, V_padded2)
+    P_2_padded_T, V_padded2 = pad_matrices(P_2.T, V)
+    P2TV = np.matmul(P_2_padded_T, V_padded2)
     LDP_const = np.square(np.linalg.norm(P2TV))
     n = E.shape[0]
-    f = np.zeros((n))
+    f = np.zeros((n-1))
     f = np.append(f, [1])
     f = f.T
-    print('f', f)
 
-    #Calulate new boundaries
-    
-    def make_square(matrix):
-        rows, cols = matrix.shape
-        max_dim = max(rows, cols)
+    #Calulate new, transformed boundaries
 
-        # If already square, return the original matrix
-        if rows == cols:
-            return matrix
-
-        # Pad the matrix with zeros to make it square
-        if rows < cols:
-            # Add rows of zeros to make the matrix square
-            zeros_to_add = max_dim - rows
-            zeros = np.zeros((zeros_to_add, cols))
-            squared_matrix = np.vstack((matrix, zeros))
-        else:
-            # Add columns of zeros to make the matrix square
-            zeros_to_add = max_dim - cols
-            zeros = np.zeros((rows, zeros_to_add))
-            squared_matrix = np.hstack((matrix, zeros))
-
-        return squared_matrix
-
-    #Q may not be square, must test to see, calculate pseudoinverse instead
-    # if it is not
-    #Q_square = make_square(Q)
-    def inverse_or_pseudo(matrix):
-        try:
-            # Try to compute the inverse
-            inv_matrix = np.linalg.inv(matrix)
-            return inv_matrix
-        except np.linalg.LinAlgError:
-            # If an error occurs, compute the pseudo-inverse
-            pseudo_inv_matrix = np.linalg.pinv(matrix)
-            return pseudo_inv_matrix
-
+    #Q may not be square, check and calculate pseudoinverse instead of
+    # inverse if it is not
     Q_minus1 = inverse_or_pseudo(Q)
-    R_t, Q_minus1 = pad_matrices(R_t, Q_minus1)
-    RQ_minus1 = np.matmul(R_t.T, Q_minus1)
-    G, RQ_minus1 = pad_matrices(G, RQ_minus1)
-    GRQ_minus1 = np.matmul(G, RQ_minus1)
+    Q_minus1_s = Q_minus1*min_S_sq
+    #print(Q_minus1_s)
+    #print("R_t.shape, Q_minus1_s.shape", R_t.shape, Q_minus1_s.shape)
+    R, Q_minus1_s = pad_matrices(R_t.T, Q_minus1_s)
+    RQ_minus1_s = np.matmul(R, Q_minus1_s)
+    print("G, RQ_minus1_s", G.shape, RQ_minus1_s.shape)
+    G, RQ_minus1_s = pad_matrices(G, RQ_minus1_s)
+    print("G, RQ_minus1_s", G)
+    print("RQ_minus1_s", RQ_minus1_s)
+    GRQ_minus1_s = np.matmul(G, RQ_minus1_s)
 
-    return(E, n, min_S_sq, LDP_const, f)
+    P1_Tv = np.matmul(P_1_padded_T, V_padded1)
+    Q_minus1, P1_Tv = pad_matrices(Q_minus1, P1_Tv)
+    Q_minus1P1_Tv = np.matmul(Q_minus1, P1_Tv)
+    R, Q_minus1P1_Tv = pad_matrices(R, Q_minus1P1_Tv)
+    RQ_minus1P1_Tv = np.matmul(R, Q_minus1P1_Tv)
+    G, RQ_minus1P1_Tv = pad_matrices(G, RQ_minus1P1_Tv)
+    GRQ_minus1P1_Tv = np.matmul(G, RQ_minus1P1_Tv)
+    #print("GRQ_minus1_s", GRQ_minus1_s.shape)
+    #print("GRQ_minus1P1_Tv", GRQ_minus1P1_Tv.shape)
+    #GRQ_minus1_s, GRQ_minus1P1_Tv = pad_to_subtract(GRQ_minus1_s, GRQ_minus1P1_Tv)
+    #upper_boundaries = GRQ_minus1_s + GRQ_minus1P1_Tv
+    h, GRQ_minus1P1_Tv = pad_to_subtract(h, GRQ_minus1P1_Tv)
+    upper_boundaries = GRQ_minus1_s
+    lower_boundaries = h - GRQ_minus1P1_Tv
+    upper_boundaries, lower_boundaries = pad_to_subtract(upper_boundaries, lower_boundaries)
+    print("upper_boundaries", upper_boundaries)
+    print("lower_boundaries", lower_boundaries)
+    #upper_boundaries, h = pad_to_subtract(upper_boundaries, h)
+
+    new_boundaries = np.array((upper_boundaries, h))
+    #print(new_boundaries)
+
+    return(E, n, min_S_sq, LDP_const, f, new_boundaries)
+
+
+def solve_for_A(A: np.array, B: np.array, X: np.array, n):
+    """Function to find optimal matrix A for PCA performed on original data 
+    matrix. Will calculate a new row for each in initial A value and update
+      the matrix with these."""
+    
+    for i in range(len(A)):
+        new_row, resid = update_a(A, B, X, i)
+        A_shape = A.shape
+        print("A shape", A.shape)
+        if new_row.shape[0] >= A.shape[1]:
+            #print("A shape", A_shape)
+            #print("new_row", new_row.shape)
+            zeros_to_add = len(new_row) - A_shape[1]
+            #print(zeros_to_add)
+            new_A = np.pad(A, pad_width=((0, 0), (0, zeros_to_add)))
+            #A = np.hstack((A, zeros))
+            print("A shape", new_A.shape)
+        A = new_A    
+        A[i] = new_row
+    
+    print(A)
+
+
+def solve_for_B(A: np.array, B: np.array, X: np.array):
+    """Function to find optimal matrix B for PCA performed on original data
+    matrix. Will calculate a new row for each initial row in B and update 
+    the matrix with these."""
+
+
+def inverse_or_pseudo(matrix):
+    try:
+        # Try to compute the inverse
+        inv_matrix = np.linalg.inv(matrix)
+        return inv_matrix
+    except np.linalg.LinAlgError:
+        # If an error occurs, compute the pseudo-inverse
+        pseudo_inv_matrix = np.linalg.pinv(matrix)
+        return pseudo_inv_matrix
+
+
+def make_square(matrix):
+    rows, cols = matrix.shape
+    max_dim = max(rows, cols)
+
+    # If already square, return the original matrix
+    if rows == cols:
+        return matrix
+
+    # Pad the matrix with zeros to make it square
+    if rows < cols:
+        # Add rows of zeros to make the matrix square
+        zeros_to_add = max_dim - rows
+        zeros = np.zeros((zeros_to_add, cols))
+        squared_matrix = np.vstack((matrix, zeros))
+    else:
+        # Add columns of zeros to make the matrix square
+        zeros_to_add = max_dim - cols
+        zeros = np.zeros((rows, zeros_to_add))
+        squared_matrix = np.hstack((matrix, zeros))
+
+    return squared_matrix
 
 
 def check_shape(n: np.array):
@@ -297,36 +346,67 @@ def LDP_to_NNLS():
     of a problem between LDP form and that of NNLS."""
 
 
+def problem_setup(data: np.array):
+    """Function takes data matrix and boundary vectors then
+    sets up needed variables for the BPCA algorithm, including
+    performing standardisation, SVD and calculation of the
+    component scores and component loading matrices"""
+
+    std_input = standardise(data)
+    #Performing SVD on standardised, mean-centered data
+    U, s, Vt = svd(std_input, full_matrices=True)
+    s = diagsvd(s, U.shape[0], Vt.shape[-1])
+
+    #Principal Axes (PAs) and Component Scores (CSs) calculated from SVD output
+    #for ||X-AB'||**2
+    CSs = np.matmul(U, s) # A (Component Scores)
+    CSs = check_shape(CSs)
+
+    sshape = s.shape[0]
+    Vtshape = Vt.shape[1]
+    if Vtshape < sshape:
+        Vt = np.pad(Vt, pad_width=((0, sshape - Vtshape ), (0,0)))
+    elif sshape < Vtshape:
+        s = np.pad(s, pad_width=((0, Vtshape-sshape), (0,0)))
+  
+    CLM = np.dot(Vt.T, np.dot(s, np.sqrt(len(data)-n))) # B (Component loading matrix)
+    CLM = check_shape(CLM)
+    PAs = Vt.T # Principal Axes
+    PAs = check_shape(PAs)
+
+    return s, CSs, CLM, PAs
+
+
+def drop_null_cols(AMatrix: np.array):
+    null_cols = []
+
+    for col in range(AMatrix.shape[1]):
+        if sum(AMatrix[:,col]) == 0:
+            null_cols.append(col)
+    matrix_out = np.delete(AMatrix, null_cols, axis=1)
+    return matrix_out
+
+
+def drop_null_rows(AMatrix: np.array):
+    null_rows = []
+
+    for row in range(AMatrix.shape[0]):
+        if sum(AMatrix[:,row]) == 0:
+            null_rows.append(row)
+    matrix_out = np.delete(AMatrix, null_rows, axis=0)
+    return matrix_out
+
+
 # Data needs to be organised as having samples in rows, variables in columns for later calculations
 data = np.array([[1, 15, 3, 4, 5, 5.2, 18, 4, 0.2, 1], [6, 7, 8, 9, 10, 3, 4, 5, 5.2, 18]]).T 
 data2 = np.array([[5, 4, 3, 3, 1, 12, 50 , 8.55, 7.2, 90], [10, 20 , 8, 7, 6, 50 , 8.55, 7.2, 90, 1]]).T
 data = np.hstack((data,data2))
+print(data.shape)
+#Number of dimensions to reduce by
 n = 1
-std_input = standardise(data)
-print("OG", std_input.shape)
-bounds_vec = np.array([[0, 0, 0, 0, 0], [8, 8, 8, 8, 8]]).T
+#Boundaries arrays
+bounds_vec = np.array(([0,0,0,0,0,0,0,0,0,0], [99,99,99,99,99,99,99,99,99,99])).T
 
-#Performing SVD on standardised, mean-centered data
-U, s, Vt = svd(std_input, full_matrices=True)
-s = diagsvd(s, U.shape[0], Vt.shape[-1])
+s, CSs, CLM, PAs = problem_setup(data)
 
-#Principal Axes (PAs) and Component Scores (CSs) calculated from SVD output
-#for ||X-AB'||**2
-CSs = np.matmul(U, s) # A (Component Scores)
-CSs = check_shape(CSs)
-
-#explained_variance = (s**2)/ (len(data) - 1)
-#print(explained_variance.shape)
-
-sshape = s.shape[0]
-Vtshape = Vt.shape[1]
-if Vtshape < sshape:
-    Vt = np.pad(Vt, pad_width=((0, sshape - Vtshape ), (0,0)))
-elif sshape < Vtshape:
-    s = np.pad(s, pad_width=((0, Vtshape-sshape), (0,0)))    
-CLM = np.dot(Vt.T, np.dot(s, np.sqrt(len(data)-n))) # B (Component loading matrix)
-CLM = check_shape(CLM)
-PAs = Vt.T # Principal Axes
-PAs = check_shape(PAs)
-
-update_a(CSs, CLM, bounds_vec, n)
+solve_for_A(CSs, CLM, bounds_vec, n)
